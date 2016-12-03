a0787b5d47b566febb7a10a9368f0bc39e33ff70
Marc Honnorat <marc.honnorat@gmail.com>
2016-12-03 18:41:43 +0100

    Revert default themes and add my own.

---
+++ themes/marc.py
@@ -0,0 +1,9 @@
+class Color(DefaultColor):
+
+    REPO_CLEAN_BG = 22    # dark green
+    REPO_CLEAN_FG = 254   # very-light grey
+    REPO_DIRTY_BG = 160   # red
+    REPO_DIRTY_FG = 254   # very-light grey
+
+    VIRTUAL_ENV_BG = 31   # a mid-tone blue
+    VIRTUAL_ENV_FG = 254  # very-light grey
